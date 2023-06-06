6b614787c62fe3d2b6db2c1ab5a5fcde3e5ec289
Antonio Roberto <antoniorwalachy@gmail.com>
2023-06-06 17:02:46 -0300

    comentário sobre instalação da lib

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk # NEEDS PIP INSTALL
 import json
 
 from scoreboard_widget import Scoreboard_Widget
